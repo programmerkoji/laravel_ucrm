383696651d5ff7847d3856698970f1748b366b3e
Koji Sato <utautaouka@gmail.com>
2023-09-09 10:22:15 +0900

    flash message

--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -3,5 +3,8 @@ import { Link } from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
+    <div v-if="$page.props.flash.message" class="bg-blue-200">
+        {{ $page.props.flash.message }}
+    </div>
     test
 </template>
